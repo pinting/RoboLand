b87f20c8396a3fdfdda52c9c6616adba0cec04e3
Tornyi Dénes <tornyi.denes@gmail.com>
2017-06-28 23:21:26 +0200

    Fixed demo and updated gitignore

--- out/www/README.md
+++
@@ -1,1 +0,0 @@
-The `www` folder contains the client. There is *no import or export* here, because the ES6 module support in the modern browsers are still in an early stage. Files in the lib directory bundled as `index.js`.
--- out/www/lib/main.js
+++
@@ -1,9 +0,0 @@
-class Robot {
-    say() {
-        alert("Beep-beep!");
-    }
-}
-/// <reference path="Robot.ts" />
-var canvas = document.getElementById("canvas");
-var robot = new Robot();
-canvas.onclick = () => robot.say();
